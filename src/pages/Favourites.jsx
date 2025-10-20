ece323aa21e6986a10f0ca2f0331253c328ebd81
aimei60 <lizali26@outlook.com>
2025-10-20 11:43:01 +0100

    moved files and added a git workflow pages file to publish on github pages

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,22 @@
+import "../css/Favourites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favourites() {
+  const { favourites } = useMovieContext();
+
+  if (favourites) {
+    return (
+      <div className="favourites">
+        <h2>Your Favourites</h2>
+        <div className="movies-grid">
+          {favourites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Favourites;
